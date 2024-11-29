9678939061b9dac3a7c1efba94c4e12f77254c39
02Adri <palmabenavidesa650@gmail.com>
2024-11-29 13:00:49 -0600

    Arreglando module 2

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 const supabaseUrl = "https://ariyyolskksxmpygpeer.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyaXl5b2xza2tzeG1weWdwZWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4NDg3NTQsImV4cCI6MjA0ODQyNDc1NH0.OPgAqRSKlNvgQquHnzETRQvbOtxRIUS_W5DsKLv03S8";
