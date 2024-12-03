336234bf0bcbc7d9fd092263cfb47619eda3bc13
02Adri <palmabenavidesa650@gmail.com>
2024-12-02 20:14:14 -0600

    Añadiendo url render 13

--- client/upload.js
+++ client/upload.js
@@ -2,28 +2,29 @@ const uploadForm = document.getElementById("uploadForm");
 
 uploadForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  
-  // Obtener el archivo seleccionado por el usuario
+
+  // Obtener el archivo seleccionado
   const fileInput = document.getElementById("fileInput").files[0];
 
-  // Verificar si se seleccionó un archivo
   if (fileInput) {
-    // Crear un FormData para enviar el archivo
     const formData = new FormData();
-    formData.append("docxFile", fileInput); // "docxFile" debe coincidir con el nombre del campo de archivo en el backend
+    formData.append("docxFile", fileInput); // "docxFile" coincide con el backend
 
-    // Enviar el archivo al backend
-    const response = await fetch("https://server-3-0q00.onrender.com/upload", {
-      method: "POST",
-      body: formData,
-    });
+    try {
+      const response = await fetch("https://server-3-0q00.onrender.com/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-    // Manejar la respuesta
-    if (response.ok) {
-      alert("Archivo subido correctamente");
-      window.location.href = "documents.html"; // Redirigir a la página de documentos
-    } else {
-      alert("Error al subir el archivo");
+      if (response.ok) {
+        alert("Archivo subido correctamente");
+        window.location.href = "documents.html"; // Redirigir a la página de documentos
+      } else {
+        alert("Error al subir el archivo");
+      }
+    } catch (error) {
+      alert("Error al conectar con el servidor express");
+      console.error(error);
     }
   } else {
     alert("Por favor, selecciona un archivo.");
